866a9874fca51057bf29c66683bd48c1ef864d93
Rick Tortora <rick@goodpup.com>
2023-12-05 15:33:17 -0500

    q4 part 2

--- q4.py
+++ q4.py
@@ -18,17 +18,17 @@ class Card:
   def winning_numbers(self)->set[int]:
     return self.answers & self.numbers
 
-  def score(self)->int:
+  def points(self)->int:
     winning_numbers = self.winning_numbers()
     if len(winning_numbers) == 0:
       return 0
     return 2 ** (len(self.winning_numbers()) - 1)
 
 def parse_card(card_text: str)->Card:
-  id, answers_text, _, numbers_text, _ = re.match(r"Card +(\d+):(( *\d+)*) \| (( *\d+)*)", card_text).groups()
+  id_text, answers_text, _, numbers_text, _ = re.match(r"Card +(\d+):(( *\d+)*) \| (( *\d+)*)", card_text).groups()
   answers = set(int(v) for v in re.split(r' +', answers_text.strip()))
   numbers = set(int(v) for v in re.split(r' +', numbers_text.strip()))
-  return Card(id, answers, numbers)
+  return Card(int(id_text), answers, numbers)
 
 def parse_cards(card_texts: str)->list[Card]:
   return list(parse_card(line) for line in card_texts.splitlines())
@@ -36,12 +36,28 @@ def parse_cards(card_texts: str)->list[Card]:
 def q1(input: str):
   cards = parse_cards(input)
   for card in cards:
-    print(f'{card} winning={card.winning_numbers()} score={card.score()}')
-  total = sum(card.score() for card in cards)
+    print(f'{card} winning={card.winning_numbers()} score={card.points()}')
+  total = sum(card.points() for card in cards)
   print(f'{total}')
+
+def q2(input: str):
+  cards = parse_cards(input)
+  cards_dict = dict((card.id, card) for card in cards)
+  copies_dict = dict((card.id, 1) for card in cards)
+  print(f'{len(cards)} cards')
+  for card in cards:
+    for i in range(0, copies_dict[card.id]):
+      for j in range(1, 1 + len(card.winning_numbers())):
+        #print(f'{card} {i} {j} {type(card.id)} {type(j)}')
+        copies_dict[card.id + j] += 1
+  print(copies_dict)
+  total = sum(count for count in copies_dict.values())
+  print(total)
 
 def main():
   q1(test_input)
   q1(open("./inputs/q4.txt").read())
+  q2(test_input)
+  q2(open("./inputs/q4.txt").read())
 
 main()
