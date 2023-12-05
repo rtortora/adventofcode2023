9477a0a84f62cb402a976977e507735e9cd20663
Rick Tortora <rick@goodpup.com>
2023-12-05 14:27:11 -0500

    q3 part 2 done

--- q3.py
+++ q3.py
@@ -81,47 +81,60 @@ class Gear:
         self.symbol = symbol
         self.part_numbers = part_numbers
 
-    def gear_ratio(self)->int:
+    def ratio(self)->int:
         if len(self.part_numbers) != 2:
             raise Exception(f"{self} is not a valid gear")
-        return self.part_numbers[0].value & self.part_numbers[1].value
+        return self.part_numbers[0].value * self.part_numbers[1].value
 
     def __repr__(self)->str:
         return f'Gear({self.symbol}, {self.part_numbers})'
 
 def main():
-    part1()
-    part2()
+    #schematic_text = test_input
+    schematic_text = open("./inputs/q3.txt").read()
+    part1(schematic_text) # 556057
+    part2(schematic_text) # 82824352
 
-def part1():
-    #schematic_scan = scan_schematic(test_input)
-    schematic_scan = scan_schematic(open("./inputs/q3.txt").read())
+def part1(schematic_text: str):
+    schematic_scan = scan_schematic(schematic_text)
     part_numbers = pull_part_numbers_adjacent_to_symbols(schematic_scan)
     total = sum(part_number.value for part_number in part_numbers)
-    print(sum)
+    print(total)
 
-def part2():
-    pass
+def part2(schematic_text: str):
+    schematic_scan = scan_schematic(schematic_text)
+    gears = pull_gears(schematic_scan)
+    for gear in gears:
+        print(f'{gear} {gear.ratio()}')
+    total = sum(gear.ratio() for gear in gears)
+    print(total)
 
 def pull_gears(schematic_scan)->list[Gear]:
     gears: list[Gear] = []
     for symbol in schematic_scan.symbols:
         if symbol.value != '*':
             continue
-        adjacent_part_numbers: list[PartNumber] = []
-
+        adjacent_part_numbers = get_adjacent_part_numbers(schematic_scan, symbol)
+        if len(adjacent_part_numbers) == 2:
+            gears.append(Gear(symbol, adjacent_part_numbers))
     return gears
 
 def pull_part_numbers_adjacent_to_symbols(schematic_scan: SchematicScan)->list[PartNumber]:
     part_numbers: dict[int, PartNumber] = {}
     for symbol in schematic_scan.symbols:
-        for point in get_adjacent_points(schematic_scan, symbol.pos):
-            for part_number in schematic_scan.part_numbers:
-                if part_number.contains_point(point):
-                    if not (part_number.instance_id in part_numbers.keys()):
-                        print(f'{symbol} {point} {part_number} {part_number.contains_point(point)}')
-                        part_numbers[part_number.instance_id] = part_number
-    return part_numbers.values()
+        for part_number in get_adjacent_part_numbers(schematic_scan, symbol):
+            part_numbers[part_number.instance_id] = part_number
+    return list(part_numbers.values())
+
+def get_adjacent_part_numbers(schematic_scan: SchematicScan, symbol: Symbol)->list[PartNumber]:
+    part_numbers: dict[int, PartNumber] = {}
+    for point in get_adjacent_points(schematic_scan, symbol.pos):
+        for part_number in schematic_scan.part_numbers:
+            if part_number.contains_point(point):
+                if not (part_number.instance_id in part_numbers.keys()):
+                    print(f'{symbol} {point} {part_number} {part_number.contains_point(point)}')
+                    part_numbers[part_number.instance_id] = part_number
+    return list(part_numbers.values())
 
 def scan_schematic(schematic_text: str) -> SchematicScan:
     next_instance_id = 1
